e5e93614dfefb5f0c6225f33d5052afd73114f3b
RITESSA <strkrita@gmail.com>
2026-02-20 17:42:55 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+
+document.getElementById('addNote').addEventListener ('click', e => createNote());
+
+function createNote() {
+    const noteDiv = document.createElement('div');
+    noteDiv.className = 'note';
+    noteDiv.id = "noteN"
+    noteDiv.innerHTML = `
+      <label class="noteItem">
+        <span class="noteCheckbox"></span>
+        <span class="labelName">Default</span>
+      </label>
+    `;
+
+    document.getElementById('noteList').insertBefore(noteDiv, document.getElementById('note2').nextSibling);
+}
+
+
+// Чтение клика по пункту, смена класса выполненности
+
+document.getElementById('noteList').addEventListener('click', function(e) {
+    if (e.target.classList.contains('noteItem')) {
+        e.target.closest('.noteItem').classList.toggle('isDone');
+    })
